4d4f782aceb0fdfbd23764737d4a9df9b3ae8eda
lipengzhou <lpzmail@163.com>
2022-01-06 10:45:17 +0800

    shopping-cart 模板配置

---
+++ shopping-cart/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Pinia - 购物车示例</h1>
+  <hr />
+  <h2>商品列表</h2>
+  <ProductList />
+  <hr />
+  <ShoppingCart />
+</template>
+
+<script setup lang="ts">
+import ProductList from './components/ProductList.vue'
+import ShoppingCart from './components/ShoppingCart.vue'
+</script>
+
+<style>
+</style>
---
+++ shopping-cart/src/components/ShoppingCart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cart">
+    <h2>你的购物车</h2>
+    <p>
+      <i>请添加一些商品到购物车.</i>
+    </p>
+    <ul>
+      <li>商品名称 - 商品价格 x 商品数量</li>
+      <li>商品名称 - 商品价格 x 商品数量</li>
+      <li>商品名称 - 商品价格 x 商品数量</li>
+    </ul>
+    <p>商品总价: xxx</p>
+    <p>
+      <button>结算</button>
+    </p>
+    <p>结算成功 / 失败.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
